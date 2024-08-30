d41a0752c6ec176426325c8c556115cd0ef85a78
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-08-30 20:26:03 +0330

    feat:complete workflow page

---
+++ src/app/(main)/(pages)/workflows/_components/workflow-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+import CustomModal from "@/components/global/custom-modal";
+import { Button } from "@/components/ui/button";
+import { useModal } from "@/providers/modal-provider";
+import { Plus } from "lucide-react";
+import React from "react";
+
+type Props = {};
+
+const WorkflowButton = (props: Props) => {
+  const { setOpen, setClose } = useModal();
+
+  return (
+    <Button size={"icon"}>
+      <Plus />
+    </Button>
+  );
+};
+
+export default WorkflowButton;
